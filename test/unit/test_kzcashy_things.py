8468694ebac16ac87ca32c38706441e03cf8bf2e
kzcashteam <33062521+kzcashteam@users.noreply.github.com>
2018-03-29 23:48:14 +0300

    valid_kzcash_address must pass tests

--- test/unit/test_kzcashy_things.py
+++ test/unit/test_kzcashy_things.py
@@ -7,12 +7,12 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../
 
 @pytest.fixture
 def valid_kzcash_address(network='mainnet'):
-    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui' if (network == 'testnet') else 'XpjStRH8SgA6PjgebtPZqCa9y7hLXP767n'
+    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui' if (network == 'testnet') else 'KuKaonuAGhTTWFQEZB8QbWB7USaB27R6mn'
 
 
 @pytest.fixture
 def invalid_kzcash_address(network='mainnet'):
-    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Uj' if (network == 'testnet') else 'XpjStRH8SgA6PjgebtPZqCa9y7hLXP767m'
+    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Uj' if (network == 'testnet') else 'KuKaonuAGhTTWFQEZB8QbWB7USaB27R6mN'
 
 
 @pytest.fixture
